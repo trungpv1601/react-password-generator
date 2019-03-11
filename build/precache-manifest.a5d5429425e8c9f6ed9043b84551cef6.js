60cf49451896885e97749bf9bde53f7de0169bf7
trungpv <trungpv1601@gmail.com>
2019-03-11 16:44:44 +0700

    reverse folder /build in .gitignore

---
+++ build/precache-manifest.a5d5429425e8c9f6ed9043b84551cef6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "936e806337c1fc35a393",
+    "url": "/static/js/main.936e8063.chunk.js"
+  },
+  {
+    "revision": "f0d5a8782b4c6078adb8",
+    "url": "/static/js/2.f0d5a878.chunk.js"
+  },
+  {
+    "revision": "936e806337c1fc35a393",
+    "url": "/static/css/main.b5fbe549.chunk.css"
+  },
+  {
+    "revision": "9209313c4e38e78d4226f0e7de309cf1",
+    "url": "/index.html"
+  }
+];
